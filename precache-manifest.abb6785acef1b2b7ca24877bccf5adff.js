6e58a34aa3b1a664ff20469790dc41f99cf06199
Govind <govindjee88@gmail.com>
2020-09-09 11:09:38 +0530

    Updates

---
+++ precache-manifest.abb6785acef1b2b7ca24877bccf5adff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cb75682c0a1375d2e6ced3658e46bd",
+    "url": "/ReactProject24/index.html"
+  },
+  {
+    "revision": "93ced1751850ed150812",
+    "url": "/ReactProject24/static/css/main.c6135b56.chunk.css"
+  },
+  {
+    "revision": "86aea3ac40de3b57d5dd",
+    "url": "/ReactProject24/static/js/2.e85683e4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ReactProject24/static/js/2.e85683e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93ced1751850ed150812",
+    "url": "/ReactProject24/static/js/main.28e97379.chunk.js"
+  },
+  {
+    "revision": "043e0e8f1721e9c5d5f6",
+    "url": "/ReactProject24/static/js/runtime-main.a1df0791.js"
+  },
+  {
+    "revision": "21e787cf5ca1a2859e40c27a40abf593",
+    "url": "/ReactProject24/static/media/about.21e787cf.jpg"
+  },
+  {
+    "revision": "7baa9a8ca956266e3931d504044e80e3",
+    "url": "/ReactProject24/static/media/banner.7baa9a8c.jpg"
+  }
+]);
